e75901dbff34d8dcd4fa9a31a3e3043c4e17da57
Steve Wallis <wallisoft@gmail.com>
2021-10-19 22:20:48 +0100

    Add files via upload

--- ovpn.sh
+++ ovpn.sh
@@ -8,10 +8,12 @@ ses=`openvpn3 sessions-list|grep Path -m1|cut -d':' -f 2`
 if [[ $ses == *"openvpn"* ]]
 then
 	cp ovpn-off.png ovpn.png
+	cp /home/steve/bin/ovpn/ovpn.ovpn.desktop.off /usr/share/applications/ovpn.ovpn.desktop
 	notify-send -i /home/steve/bin/ovpn/ovpn-off.png 'Open VPN Disconnected'
 	openvpn3 session-manage --session-path $ses --disconnect
 else
 	cp ovpn-on.png ovpn.png
+	cp /home/steve/bin/ovpn/ovpn.ovpn.desktop.on /usr/share/applications/ovpn.ovpn.desktop
 	notify-send -i /home/steve/bin/ovpn/ovpn-on.png 'Open VPN Connected'
 	nohup openvpn3 session-start --config /home/steve/bin/ovpn/steve_w10.ovpn &
 fi

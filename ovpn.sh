bcff7f356c4d8cb5c4522116a98e88d79dc6fd8c
Steve Wallis <wallisoft@gmail.com>
2021-10-28 02:30:51 +0100

    Add files via upload
    Added installer and add to favorites

--- ovpn.sh
+++ ovpn.sh
@@ -3,22 +3,40 @@
 #steve wallis 01/10/2021
 
 
-profile="/home/steve/bin/ovpn/steve_w10.ovpn"
+PROFILE=
+INSTALLDIR=
 
 
-
-#call with -startup in startup applications to reconnect at boot if connected at shutdown.
 
 if [[ $1 == "-startup" ]]
 then
-    echo "startup"
-    if ! grep -q Disconnected /usr/share/applications/ovpn.ovpn.desktop
+
+    #call with -startup in startup applications to reconnect at boot if connected at shutdown.
+
+    if ! grep -q Disconnected ~/.local/share/applications/ovpn-sh.desktop
     then
-        nohup openvpn3 session-start --config $profile &
+        nohup openvpn3 session-start --config $PROFILE &
     fi
     exit
-fi
 
+elif [[ $1 == "-config" ]]
+then
+
+    #change/set ovpn connection profile
+    
+    FILE=`zenity --file-selection --title="Select OpenVPN Profile (usually *.ovpn)"`
+
+    case $? in
+         0)
+             sed -i "/^PROFILE=/c\PROFILE=\"$FILE\"" $INSTALLDIR/ovpn.sh
+             notify-send -i $INSTALLDIR/ovpn-off.png 'OpenVPN Profile Changed';;
+         1)
+             notify-send -i $INSTALLDIR/ovpn-off.png 'No OpenVPN Profile Selected';;
+         -1)
+             notify-send -i $INSTALLDIR/ovpn-off.png 'OpenVPN Unexpected Error';;
+    esac
+    exit
+fi
 
 
 #toggle connect/disconnect
@@ -27,14 +45,12 @@ ses=`openvpn3 sessions-list|grep Path -m1|cut -d':' -f 2`
 
 if [[ $ses == *"openvpn"* ]]
 then
-	cp ovpn-off.png ovpn.png
-	cp /home/steve/bin/ovpn/ovpn.ovpn.desktop.off /usr/share/applications/ovpn.ovpn.desktop
-	notify-send -i /home/steve/bin/ovpn/ovpn-off.png 'Open VPN Disconnected'
+	cp $INSTALLDIR/ovpn-sh.desktop.off ~/.local/share/applications/ovpn-sh.desktop
+	notify-send -i $INSTALLDIR/ovpn-off.png 'Open VPN Disconnected'
 	openvpn3 session-manage --session-path $ses --disconnect
 else
-	cp ovpn-on.png ovpn.png
-	cp /home/steve/bin/ovpn/ovpn.ovpn.desktop.on /usr/share/applications/ovpn.ovpn.desktop
-	notify-send -i /home/steve/bin/ovpn/ovpn-on.png 'Open VPN Connected'
-	nohup openvpn3 session-start --config $profile &
+	cp $INSTALLDIR/ovpn-sh.desktop.on ~/.local/share/applications/ovpn-sh.desktop
+	notify-send -i $INSTALLDIR/ovpn-on.png 'Open VPN Connected'
+	nohup openvpn3 session-start --config $PROFILE &
 fi
 
